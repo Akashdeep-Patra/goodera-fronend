5ee8a6a0b6810c40d461ab1e4890bee08f9e93b3
Akashdeep <adeep8961@gmail.com>
2021-10-30 13:03:04 +0530

    search

--- src/pages/FindJobs/index.tsx
+++ src/pages/FindJobs/index.tsx
@@ -32,20 +32,56 @@ const axiosinstance = axios.create({
 
 const FindJobs = () => {
   const [jobs, setJobs] = useState<Job[]>([]);
+  const [keyword, setKeyword] = useState('');
+  const [location, setLocation] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
   useEffect(() => {
     (async () => {
       const { data } = await axiosinstance.get<Job[]>('/jobs');
       setJobs(data);
     })();
   }, []);
-
+  const handleSearch = async () => {
+    setIsSearching(true);
+    try {
+      const { data } = await axiosinstance.get<Job[]>('/jobs', {
+        params: { title: keyword, location },
+      });
+      setJobs(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSearching(false);
+    }
+  };
   return (
     <div className='find-jobs'>
+      <div className='search'>
+        <input
+          placeholder='Keyword'
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          className='keyword'
+        />
+        <input
+          placeholder='Location'
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          className='location'
+        />
+        <button
+          disabled={isSearching}
+          onClick={handleSearch}
+          className='search-button'
+        >
+          search
+        </button>
+      </div>
       {jobs && (
         <div className='jobs'>
           {jobs.map((job) => (
-            <div>
-              <Card key={job.id} {...job} />
+            <div key={job.id}>
+              <Card {...job} />
             </div>
           ))}
         </div>
